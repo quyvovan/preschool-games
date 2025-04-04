41b50fa1793d1ff3bfa7300246f2733198182b3f
quyvovan <vanquy538@gmail.com>
2025-04-04 22:22:02 +0700

    feat: add link game

--- src/containers/landing-home/index.tsx
+++ src/containers/landing-home/index.tsx
@@ -1,5 +1,12 @@
 import { Box } from '@mui/material';
+import Link from 'next/link';
 
 export const LandingMainPageContainer = () => {
-  return <Box>LandingMainPageContainer</Box>;
+  return (
+    <Box>
+      <Link href="/game">
+        <h1>Go to Game</h1>
+      </Link>
+    </Box>
+  );
 };
